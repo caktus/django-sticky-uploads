81f4c1bf119b46c0716b0565c216e210960fc250
Mark Lavin <mlavin@caktusgroup.com>
2013-07-21 14:50:56 -0400

    Use the url as a namespace for the serialization.

--- stickyuploads/utils.py
+++ stickyuploads/utils.py
@@ -8,9 +8,9 @@ from django.core.files.storage import get_storage_class
 from django.utils.functional import LazyObject
 
 
-def serialize_upload(name, storage):
+def serialize_upload(name, storage, url):
     """
-    Serialize uploaded file by name and storage.
+    Serialize uploaded file by name and storage. Namespaced by the upload url.
     """
     if isinstance(storage, LazyObject):
         # Unwrap lazy storage class
@@ -21,16 +21,16 @@ def serialize_upload(name, storage):
     return signing.dumps({
         'name': name,
         'storage': '%s.%s' % (cls.__module__, cls.__name__)
-    })
+    }, salt=url)
 
 
-def deserialize_upload(value):
+def deserialize_upload(value, url):
     """
-    Restore file and name and storage from serialized value.
+    Restore file and name and storage from serialized value and the upload url.
     """
     result = {'name': None, 'storage': None}
     try:
-        result = signing.loads(value)
+        result = signing.loads(value, salt=url)
     except signing.BadSignature:
         # TODO: Log invalid signature
         pass
@@ -43,12 +43,13 @@ def deserialize_upload(value):
     return result
 
 
-def open_stored_file(value):
+def open_stored_file(value, url):
     """
-    Deserialize value and return open file. Returns None if deserialization fails.
+    Deserialize value for a given upload url and return open file.
+    Returns None if deserialization fails.
     """
     upload = None
-    result = deserialize_upload(value)
+    result = deserialize_upload(value, url)
     filename = result['name']
     storage_class = result['storage']
     if storage_class and filename:
