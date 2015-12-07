cefcbc89bdd95e60e66039794b6c6416cc29f518
bphillips <bphillips@caktusgroup.com>
2015-12-07 11:48:28 -0500

    Fixed Django1.10 deprecations and updated tox.ini

--- stickyuploads/tests/test_forms.py
+++ stickyuploads/tests/test_forms.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.core.files.uploadedfile import SimpleUploadedFile
-from django.utils import unittest
+import unittest
 
 from mock import patch
 
