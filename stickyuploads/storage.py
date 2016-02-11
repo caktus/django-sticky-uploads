7559b851f6e271e767dd5b61699dbbe973e0846f
Venelin Stoykov <venelin@magicsolutions.bg>
2016-02-11 15:38:27 +0200

    Put files in random directories to prevent filename collisions (Fixes #14)
    This will also helps in security because will make filename guessing harder.

--- stickyuploads/storage.py
+++ stickyuploads/storage.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 
 from django.core.files.storage import FileSystemStorage
@@ -13,3 +14,17 @@ class TempFileSystemStorage(FileSystemStorage):
     def url(self, name):
         """Files are not accessable via url."""
         raise NotImplementedError()
+
+    def get_available_name(self, name, max_length=None):
+        """Return relative path to name placed in random directory"""
+        tempdir = tempfile.mkdtemp(dir=self.base_location)
+        name = os.path.join(
+            os.path.basename(tempdir),
+            os.path.basename(name),
+        )
+        method = super(TempFileSystemStorage, self).get_available_name
+        try:
+            return method(name, max_length=max_length)
+        except TypeError:
+            # Django < 1.8
+            return method(name)
